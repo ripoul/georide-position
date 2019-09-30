171ddf5db1259b1d5ce087107ea9b08a64bce7a4
Jules LE BRIS <jls.lebris@gmail.com>
2019-09-30 11:57:27 +0200

    update setting

--- georidePosition/settings.py
+++ georidePosition/settings.py
@@ -119,7 +119,8 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
-STATIC_URL = "/static/"
+STATIC_URL = '/static/'
+STATIC_ROOT = 'static'
 
 if os.getenv("GAE_ENV", "").startswith("standard"):
     MAINTENANCE_MODE = True
