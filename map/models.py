18f7ee312f13fde18d0aaa417419dc5266144597
Jules LE BRIS <jls.lebris@gmail.com>
2019-10-10 10:20:45 +0200

    init v3 : - create model - update req - update travis : migrate model - add first mig

--- map/models.py
+++ map/models.py
@@ -1,3 +1,10 @@
 from django.db import models
+from django.contrib.auth.models import User
+from fernet_fields import EncryptedTextField
 
 # Create your models here.
+class Profile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)  # name, email, password
+    startDate = models.DateTimeField()
+    endDate = models.DateTimeField()
+    token = EncryptedTextField()
