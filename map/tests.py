80221b8dbfda56e9ca49fc9b2f6e749ab38bc5c2
Jules LE BRIS <jls.lebris@gmail.com>
2019-10-19 16:41:14 +0200

    add fake test

--- map/tests.py
+++ map/tests.py
@@ -2,12 +2,22 @@ from django.test import TestCase, Client
 
 # Create your tests here.
 class MapCase(TestCase):
-    def test_map_display(self):
+    def test_index(self):
         c = Client()
         response = c.get("/")
         self.assertEqual(response.status_code, 200, "unexpected return code")
 
-    def test_get_position(self):
+    def test_create_form(self):
         c = Client()
-        response = c.post("/positions")
+        response = c.get("/create")
+        self.assertEqual(response.status_code, 200, "unexpected return code")
+
+    def test_modifie_form(self):
+        c = Client()
+        response = c.get("/modifie")
+        self.assertEqual(response.status_code, 302, "unexpected return code")
+
+    def test_connect_form(self):
+        c = Client()
+        response = c.get("/connect")
         self.assertEqual(response.status_code, 200, "unexpected return code")
