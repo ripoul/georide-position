9712bf92da057b2fcdc84ec271f9e594f6a788f6
Jules LE BRIS <jls.lebris@gmail.com>
2019-10-31 13:49:59 +0100

    :recycle: use views decorators to remove if (#63)

--- map/views.py
+++ map/views.py
@@ -11,6 +11,8 @@ from django.urls import reverse
 from django.http import HttpResponse
 from .utils import get_vars
 from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth.decorators import login_required
+from django.views.decorators.http import require_http_methods
 
 
 class georide_cli:
@@ -52,12 +54,14 @@ class georide_cli:
 geo = georide_cli()
 
 
+@require_http_methods(["GET"])
 def index(request):
     profiles = Profile.objects.filter
     param = {"connected": request.user.is_authenticated, "profiles": profiles}
     return render(request, "map/index.html", param)
 
 
+@require_http_methods(["GET"])
 def road_trip(request, username):
     user = User.objects.get(username=username)
     profile = Profile.objects.get(user=user)
@@ -70,6 +74,7 @@ def road_trip(request, username):
     return render(request, "map/road-trip.html", param)
 
 
+@require_http_methods(["GET"])
 def getPositions(request, username):
     startDateReq = request.GET.get("startDate", None)
     endDateReq = request.GET.get("endDate", None)
@@ -89,165 +94,160 @@ def getPositions(request, username):
     return HttpResponse([], content_type="application/json")
 
 
+@require_http_methods(["GET"])
 def getInfo(request):
     param = {"connected": request.user.is_authenticated}
     return render(request, "map/get-info.html", param)
 
 
+@require_http_methods(["POST"])
 def getToken(request):
-    if request.method == "POST":
-        email = request.POST.get("email")
-        password = request.POST.get("password")
-        if not email or not password:
-            return HttpResponse(status=400)
-        token = geo.getNewToken(email, password)
-        ret = {"token": token}
-        return HttpResponse(json.dumps(ret), content_type="application/json")
-    return HttpResponse(status=405)
+    email = request.POST.get("email")
+    password = request.POST.get("password")
+    if not email or not password:
+        return HttpResponse(status=400)
+    token = geo.getNewToken(email, password)
+    ret = {"token": token}
+    return HttpResponse(json.dumps(ret), content_type="application/json")
 
 
+@require_http_methods(["POST"])
 def getTrackers(request):
-    if request.method == "POST":
-        token = request.POST.get("token")
-        if not token:
-            return HttpResponse(status=400)
-        ret = geo.getTrackersID(token)
-        return HttpResponse(json.dumps(ret), content_type="application/json")
-    return HttpResponse(status=405)
+    token = request.POST.get("token")
+    if not token:
+        return HttpResponse(status=400)
+    ret = geo.getTrackersID(token)
+    return HttpResponse(json.dumps(ret), content_type="application/json")
 
 
+@require_http_methods(["GET"])
 def createAccountForm(request):
     if request.user.is_authenticated:
         return redirect(reverse(road_trip, args=[request.user.username]))
     return render(request, "map/create-account.html", {})
 
 
+@require_http_methods(["POST"])
 def createAccount(request):
-    if request.method == "POST":
-        username = request.POST.get("id")
-        email = request.POST.get("email")
-        token = request.POST.get("token")
-        trackerID = request.POST.get("trackerID")
-        password = request.POST.get("password")
-        startDate = request.POST.get("startDate")
-        endDate = request.POST.get("endDate")
-        if (
-            not username
-            or not email
-            or not token
-            or not trackerID
-            or not password
-            or not startDate
-            or not endDate
-        ):
-            return HttpResponse(status=400)
-        trackerID = int(trackerID)
-        # check date
-        try:
-            profile = Profile.objects.create_profile(
-                username, email, password, token, trackerID, startDate, endDate
-            )
-            profile.save()
-            user = authenticate(request, username=username, password=password)
-            if user is not None:
-                login(request, user)
-            return HttpResponse(status=202)
-        except IntegrityError:
-            return HttpResponse(status=500)
-    return HttpResponse(status=405)
+    username = request.POST.get("id")
+    email = request.POST.get("email")
+    token = request.POST.get("token")
+    trackerID = request.POST.get("trackerID")
+    password = request.POST.get("password")
+    startDate = request.POST.get("startDate")
+    endDate = request.POST.get("endDate")
+    if (
+        not username
+        or not email
+        or not token
+        or not trackerID
+        or not password
+        or not startDate
+        or not endDate
+    ):
+        return HttpResponse(status=400)
+    trackerID = int(trackerID)
+    # check date
+    try:
+        profile = Profile.objects.create_profile(
+            username, email, password, token, trackerID, startDate, endDate
+        )
+        profile.save()
+        user = authenticate(request, username=username, password=password)
+        if user is not None:
+            login(request, user)
+        return HttpResponse(status=202)
+    except IntegrityError:
+        return HttpResponse(status=500)
 
 
+@require_http_methods(["GET"])
 def connectAccountForm(request):
     if request.user.is_authenticated:
         return redirect(reverse(road_trip, args=[request.user.username]))
     return render(request, "map/connect-account.html", {})
 
 
+@login_required
+@require_http_methods(["GET"])
 def disconect(request):
-    if request.user.is_authenticated:
-        username = request.user.username
-        logout(request)
-        return redirect(reverse(road_trip, args=[username]))
-    return redirect(reverse(index))
+    username = request.user.username
+    logout(request)
+    return redirect(reverse(road_trip, args=[username]))
 
 
+@require_http_methods(["POST"])
 def connectAccount(request):
-    if request.method == "POST":
-        username = request.POST.get("id")
-        password = request.POST.get("password")
-        if not username or not password:
-            return HttpResponse(status=400)
-        user = authenticate(request, username=username, password=password)
-        if user is not None:
-            login(request, user)
-        else:
-            return HttpResponse(status=500)
-        return HttpResponse(status=202)
-    return HttpResponse(status=405)
+    username = request.POST.get("id")
+    password = request.POST.get("password")
+    if not username or not password:
+        return HttpResponse(status=400)
+    user = authenticate(request, username=username, password=password)
+    if user is not None:
+        login(request, user)
+    else:
+        return HttpResponse(status=500)
+    return HttpResponse(status=202)
 
 
+@login_required
+@require_http_methods(["GET"])
 def modifyAccountForm(request):
-    if request.user.is_authenticated:
-        user = request.user
-        profile = Profile.objects.get(user=user)
-        startDate = profile.startDate.strftime("%Y-%m-%d")
-        endDate = profile.endDate.strftime("%Y-%m-%d")
-        param = {
-            "id": user.username,
-            "email": user.email,
-            "startDate": startDate,
-            "endDate": endDate,
-            "token": profile.token,
-            "trackerID": profile.trackerID,
-            "connected": request.user.is_authenticated,
-        }
-        return render(request, "map/modify-account.html", param)
-    return redirect(reverse(connectAccountForm))
+    user = request.user
+    profile = Profile.objects.get(user=user)
+    startDate = profile.startDate.strftime("%Y-%m-%d")
+    endDate = profile.endDate.strftime("%Y-%m-%d")
+    param = {
+        "id": user.username,
+        "email": user.email,
+        "startDate": startDate,
+        "endDate": endDate,
+        "token": profile.token,
+        "trackerID": profile.trackerID,
+        "connected": request.user.is_authenticated,
+    }
+    return render(request, "map/modify-account.html", param)
 
 
+@login_required
+@require_http_methods(["POST"])
 def modifyAccount(request):
-    if request.method == "POST":
-        if request.user.is_authenticated:
-            user = request.user
-            profile = Profile.objects.get(user=user)
+    user = request.user
+    profile = Profile.objects.get(user=user)
 
-            username = request.POST.get("id")
-            email = request.POST.get("email")
-            token = request.POST.get("token")
-            trackerID = request.POST.get("trackerID")
-            startDate = request.POST.get("startDate")
-            endDate = request.POST.get("endDate")
+    username = request.POST.get("id")
+    email = request.POST.get("email")
+    token = request.POST.get("token")
+    trackerID = request.POST.get("trackerID")
+    startDate = request.POST.get("startDate")
+    endDate = request.POST.get("endDate")
 
-            if (
-                not username
-                or not email
-                or not token
-                or not trackerID
-                or not startDate
-                or not endDate
-            ):
-                return HttpResponse(status=400)
+    if (
+        not username
+        or not email
+        or not token
+        or not trackerID
+        or not startDate
+        or not endDate
+    ):
+        return HttpResponse(status=400)
 
-            user.username = username
-            user.email = email
-            user.save()
+    user.username = username
+    user.email = email
+    user.save()
 
-            profile.token = token
-            profile.trackerID = trackerID
-            profile.startDate = startDate
-            profile.endDate = endDate
-            profile.save()
-            return HttpResponse(status=202)
-        return HttpResponse(status=403)
-    return HttpResponse(status=405)
+    profile.token = token
+    profile.trackerID = trackerID
+    profile.startDate = startDate
+    profile.endDate = endDate
+    profile.save()
+    return HttpResponse(status=202)
 
 
+@login_required
+@require_http_methods(["POST"])
 def revokeToken(request):
-    if request.method == "POST":
-        if request.user.is_authenticated:
-            user = request.user
-            profile = Profile.objects.get(user=user)
-            token = profile.token
-            return HttpResponse(status=geo.revokeToken(token))
-        return HttpResponse(status=403)
-    return HttpResponse(status=405)
+    user = request.user
+    profile = Profile.objects.get(user=user)
+    token = profile.token
+    return HttpResponse(status=geo.revokeToken(token))
